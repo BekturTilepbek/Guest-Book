5cc3a8fd40c703ea8ecf02f8ea68b9a9c22d40a2
Bektur <project.bekturtilepbek@gmail.com>
2024-07-06 14:20:25 +0600

    настроил проект, добавил модель записи, зарегистрировал, добавил шаблоны

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Entry
+
+
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'created_at', 'updated_at', 'status']
+    list_display_links = ['id', 'name']
+    list_filter = ['status']
+    search_fields = ['name']
+    fields = ['name', 'email', 'text', 'status']
+
+
+admin.site.register(Entry, EntryAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+statuses = [('active', 'Активно'), ('blocked', 'Заблокировано')]
+
+
+class Entry(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, verbose_name="Имя автора записи")
+    email = models.EmailField(null=False, blank=False, verbose_name="Почта автора записи")
+    text = models.TextField(null=False, blank=False, verbose_name="Текст записи")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата и время редактирования")
+    status = models.CharField(max_length=20, choices=statuses, default='active', verbose_name="Статус")
+
+    def __str__(self):
+        return f"{self.pk}. {self.name} ({self.status})"
+
+    class Meta:
+        db_table = "entries"
+        verbose_name = "Запись"
+        verbose_name_plural = "Записи"
